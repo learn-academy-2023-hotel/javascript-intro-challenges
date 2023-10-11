815ae1b390b70ae840442355c821b58d2e3157d3
alexnguyen <alexanderdnguyen4@gmail.com>
2023-10-11 13:44:42 -0700

    First half of questions done

---
+++ intro-sam-alex.js
@@ -0,0 +1,20 @@
+// Write the code that will log the output of 34 added to 71.
+
+console.log(34 + 71)
+
+// Write the code that will log the output of 67 subtracted from 123.
+
+console.log(123 - 67)
+
+// Write the code that will log the output of 56 multiplied by 23.
+
+console.log(56 * 23)
+
+// Write the code that will log the output of 45 divided by 5.
+
+console.log(45 / 5)
+
+// Write the code that will log the output of 5 to the power of 7.
+
+console.log(5 ** 7)
+
