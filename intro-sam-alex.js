b9e5d7cdd0b48448f200044267c91ffd6b6098e5
SlaBrutte <samlabrutte@yahoo.com>
2023-10-11 14:01:54 -0700

    Yay we did it

--- intro-sam-alex.js
+++ intro-sam-alex.js
@@ -18,3 +18,19 @@ console.log(45 / 5)
 
 console.log(5 ** 7)
 
+// Write the code that will log the whole number remainder of 33 divided by 6.
+console.log(33 % 6)
+
+// Write the code that will log the length of a string containing your name.
+string = "Samuel"
+console.log(string.length)
+
+// Write the code that will log whether your string includes the letter "e"?
+console.log(string.includes("e"))
+
+// Write the code that will log the character at the first index of the string.
+console.log(string.charAt(0))
+console.log(string[0])
+
+// Write the code that will log the string in all uppercase letters.
+console.log(string.toUpperCase())
