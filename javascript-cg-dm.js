a01aee3746268498cb8133e0a632908d20e1c218
chandlergipson <chandlergipson96@gmail.com>
2023-10-11 16:35:35 -0700

    added more lines

--- javascript-cg-dm.js
+++ javascript-cg-dm.js
@@ -1,1 +1,15 @@
 console.log(34 + 71)
+
+console.log(67 - 123)
+
+console.log(56 * 23)
+
+console.log(45 / 5)
+
+console.log(5 ** 7)
+
+console.log(33 % 6)
+
+console.log("chandler".length)
+var name = chandler
+console.log(name.charAt("e"))
