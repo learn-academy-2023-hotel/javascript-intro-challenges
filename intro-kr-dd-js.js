49e6dfb331762dbc29a1c3dda4ba6a10b420ba37
kevinrojas <k3vinr07@gmail.com>
2023-10-11 13:39:59 -0700

    wrote the first lines of code, you're up

---
+++ intro-kr-dd-js.js
@@ -0,0 +1,21 @@
+// Write the code that will log the output of 34 added to 71.
+// Write the code that will log the output of 67 subtracted from 123.
+// Write the code that will log the output of 56 multiplied by 23.
+// Write the code that will log the output of 45 divided by 5.
+// Write the code that will log the output of 5 to the power of 7.
+// Write the code that will log the whole number remainder of 33 divided by 6.
+// Write the code that will log the length of a string containing your name.
+// Write the code that will log whether your string includes the letter "e"?
+// Write the code that will log the character at the first index of the string.
+// Write the code that will log the string in all uppercase letters.
+
+console.log(34+71)
+console.log(123-67)
+console.log(56*23)
+console.log(45/5)
+console.log(5**7)
+console.log(33%5)
+console.log("kevin".length)
+console.log("kevin".includes("e"))
+console.log("kevin"[0])
+console.log("kevin".toUpperCase())
