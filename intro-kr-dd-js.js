0e9ae46f25fc96c5a3c7c1e98bd9822bff68bc43
kevinrojas <k3vinr07@gmail.com>
2023-10-11 16:11:27 -0700

    updated first part of challenge

--- intro-kr-dd-js.js
+++ intro-kr-dd-js.js
@@ -52,3 +52,26 @@
 // console.log(theQuestion + theAnswer)
 
 // console.log(theQuestion.indexOf("l"))
+
+// Is 34 divided by 3 greater than 67 divided by 2?
+// Does 5 evaluate to the same as "5"?
+// Does 5 strictly equal "5"?
+// Does !3 strictly equal 3?
+// Does "LEARN".length strictly equal 5 AND "Student".length strictly equal 7?
+// Does "LEARN".length strictly equal 5 OR "Student".length strictly equal 10?
+// Does "LEARN" contain the subset "RN"?
+// Does "LEARN" contain the subset "rn"?
+// Does "LEARN"[0] strictly equal "l"?
+// Modify the code from the previous question to return true.
+
+console.log(34/3 > 67/2)
+console.log(5 == '5')
+console.log(5 === '5') //false
+console.log(!3 === 3) //false
+console.log("LEARN".length === 5 && "Student".length === 7) //true
+console.log("LEARN".length === 5 || "student".length === 10 ) // true
+console.log("LEARN".includes("RN")) //true
+console.log("LEARN".includes("rn")) // false
+console.log("LEARN"[0] === 'l') // false
+console.log("LEARN"[0] !== 'l') // true
+
