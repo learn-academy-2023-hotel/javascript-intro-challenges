e0b9b592740a5d81ef74d8647782ccc9452725d4
AragonJ13 <jeanaragon0@gmail.com>
2023-10-12 15:26:51 -0700

    LOOPS

---
+++ loops-andre-jean.js
@@ -0,0 +1,25 @@
+// 1st Loop Challange 
+// Create a for loop that logs each number from 1 - 20.
+// const myNum = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
+// for (let i = 0; i < myNum.length; i++){
+//     console.log(myNum[i])
+// }
+// Create a for loop that logs every other number from 1 - 20.
+// const myNum = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
+// for (let i = 0; i < myNum.length; i+= 2){
+//     console.log(myNum[i])
+// }
+// Create a for loop that logs the result of each number from 1 - 20 tripled.
+// const myNum = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
+//     for (let i = 0; i < myNum.length; i++ ){
+//     console.log(myNum[i] * 3)
+
+// Create a for loop that logs each even number from 1-20, and in the place of every odd number, returns the word "ODD".
+// Expected output: ODD, 2, ODD, 4, ODD, 6 ...etc
+// const myNum = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
+//     for (let i = 0; i <= myNum.length; i++ ){
+//         if (myNum[i] % 2 !== 0) {
+//            console.log(i)
+//         } else 
+//            console.log("ODD")
+//     }
