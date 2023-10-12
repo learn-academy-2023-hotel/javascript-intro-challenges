8aa9bb738ff95e7a59b8f93d04a230737e524587
AragonJ13 <jeanaragon0@gmail.com>
2023-10-12 11:31:22 -0700

    array challanges 1

---
+++ practice-andre-jean.js
@@ -0,0 +1,17 @@
+// // 1st chanllenge 
+// Write the code that will add "soda" to the end of the original array.
+// var groceryList = ["chips","dip","cookies"]
+// console.log(groceryList.push("soda"))
+// console.log(groceryList)
+// Write the code that will add "granola" to the end of the array without altering the original array.
+// var groceryList = ["chips","dip","cookies"]
+// console.log(groceryList.push("granola"))
+// console.log(groceryList)
+// Write the code that will return a subset of the array containing only "chips" and "dip".
+// var groceryList = ["chips","dip","cookies"]
+// console.log(groceryList.pop("cookies"))
+// console.log(groceryList)
+// Write the code that will add "beans" to the "chips" and "dip" array.
+// var groceryList = ["chips","dip","cookies"]
+// console.log(groceryList.pop("cookies"),groceryList.push("beans"))
+// console.log(groceryList)
