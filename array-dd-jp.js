ad2c62743bd53cea78af0073917d397bc61b2e69
Danny De La Rosa <danny.devlarosa@gmail.com>
2023-10-12 11:57:15 -0700

    second part of the challenge complete

--- array-dd-jp.js
+++ array-dd-jp.js
@@ -9,4 +9,35 @@ var groceryList = ["chips", "dip", "cookies"]
 // groceryList.pop()
 // groceryList.push("beans")
 
-console.log(groceryList)
+// console.log(groceryList)
+
+// Write the code that will add the number 0 to the beginning of the array.
+// Write the code that will add the number 12 to the end of the array.
+// Write the code that will remove the first number from the array.
+// Write the code that will add the number 0 to the beginning of the array without altering the original array. HINT: it's not .unshift You'll have to get creative! ;)
+
+var numbers = [, 2, 4, 6, 8, 10]
+var numberZero = [0]
+var result = (numberZero + numbers)
+
+// numbers.push(0)
+// numbers.reverse()
+// console.log(numbers)
+
+// [ 0, 10, 8, 6, 4, 2 ]
+
+// numbers.push(12)
+// console.log(numbers)
+
+// [ 2, 4, 6, 8, 10, 12 ]
+
+// numbers.reverse()
+// numbers.pop()
+// numbers.reverse()
+// console.log(numbers)
+
+// [ 4, 6, 8, 10 ]
+
+console.log(result)
+
+// 0,2,4,6,8,10
