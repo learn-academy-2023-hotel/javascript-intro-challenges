acbb9db7ba5a0387150c281d7d34b6813ad9526f
EWinkeljohn <emcwink@gmail.com>
2023-10-12 12:05:35 -0700

    second code

--- arrays-ron-emery.js
+++ arrays-ron-emery.js
@@ -1,32 +1,47 @@
-var groceryList = ["chips", "dip", "cookies"]
+// var groceryList = ["chips", "dip", "cookies"]
 
-// Write the code that will add "soda" to the end of the original array.
-groceryList.push("soda")
-console.log(groceryList)
-
-// Write the code that will add "granola" to the end of the array without altering the original array.
-console.log(groceryList.concat(" ,gronola"))
-console.log(groceryList)
-
-// Write the code that will return a subset of the array containing only "chips" and "dip".
-console.log(groceryList.slice(0, 2))
+// // Write the code that will add "soda" to the end of the original array.
+// groceryList.push("soda")
 // console.log(groceryList)
 
-// Write the code that will add "beans" to the "chips" and "dip" array.
-// console.log(groceryList.slice(0, 2).push("beans"))
-var subList = groceryList.slice(0, 2)
-console.log(subList.push("beans"))
-console.log(subList)
+// // Write the code that will add "granola" to the end of the array without altering the original array.
+// console.log(groceryList.concat(" ,gronola"))
+// console.log(groceryList)
+
+// // Write the code that will return a subset of the array containing only "chips" and "dip".
+// console.log(groceryList.slice(0, 2))
+// // console.log(groceryList)
+
+// // Write the code that will add "beans" to the "chips" and "dip" array.
+// // console.log(groceryList.slice(0, 2).push("beans"))
+// var subList = groceryList.slice(0, 2)
+// console.log(subList.push("beans"))
+// console.log(subList)
 
 // console.log(typeof subList)
 
 // Consider the variable:
 
-// var numbers = [2, 4, 6, 8, 10]
+var numbers = [2, 4, 6, 8, 10]
+
 // Write the code that will add the number 0 to the beginning of the array.
+console.log(numbers.unshift(0))
+console.log(numbers)
+
 // Write the code that will add the number 12 to the end of the array.
+console.log(numbers.push(12))
+console.log(numbers)
+
 // Write the code that will remove the first number from the array.
+console.log(numbers.shift())
+console.log(numbers)
+
 // Write the code that will add the number 0 to the beginning of the array without altering the original array. HINT: it's not .unshift You'll have to get creative! ;)
+var num = [0]
+console.log(num)
+console.log(num.concat(numbers))
+console.log(numbers)
+
 // Consider the variable:
 
 // var numSet = [2, 13, 6, 8, 4, 2]
