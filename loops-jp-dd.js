8451a27d92b6dce9aba3f395af36a0c498c51552
JacksonPruett <jacksonc.pruett@gmail.com>
2023-10-12 16:55:13 -0700

    A work in progress?

--- loops-jp-dd.js
+++ loops-jp-dd.js
@@ -5,3 +5,52 @@
 //         console.log("ODD")
 //     }
 // }
+
+
+const nums = [3, 57, -9, 20, 67];
+let biggerNumber = [0]
+
+for (let i = 0; i < nums.length; i++){
+    if (nums[1] > nums[i+1]){
+        
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let largestNumber = nums[0];
+
+// for (let i = 1; i < nums.length; i++) {
+//     if (nums[i] > largestNumber) {
+//         largestNumber = nums[i];
+//     }
+// }
+
+// console.log(largestNumber);
