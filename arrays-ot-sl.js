295abd88b9a76f825c781c53c276be78cc668983
SlaBrutte <samlabrutte@yahoo.com>
2023-10-12 11:30:22 -0700

    hello

---
+++ arrays-ot-sl.js
@@ -0,0 +1,17 @@
+// Write the code that will add "soda" to the end of the original array.
+// Write the code that will add "granola" to the end of the array without altering the original array.
+// Write the code that will return a subset of the array containing only "chips" and "dip".
+// Write the code that will add "beans" to the "chips" and "dip" array.
+
+//var groceryList = ["chips", "dip", "cookies"]
+
+// groceryList.push("soda")
+// console.log(groceryList)
+
+//groceryList.push("granola")
+// console.log(groceryList)
+
+// console.log(groceryList.slice(0, 2))
+
+// groceryList.push("beans")
+// console.log(groceryList)
