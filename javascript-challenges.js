426d1cdafd467129288a2dc40e4b269dbf80d86b
adriancarranza <adriancarranza0113@gmail.com>
2023-10-12 15:33:28 -0700

    general fix

--- javascript-challenges.js
+++ javascript-challenges.js
@@ -1,22 +1,23 @@
-// Write the code that will log the output of 34 added to 71.
-console.log(34 + 71)
-// Write the code that will log the output of 67 subtracted from 123.
-console.log(123 - 67)
-// Write the code that will log the output of 56 multiplied by 23.
-console.log(56 * 23)
-// Write the code that will log the output of 45 divided by 5.
-console.log(45 / 5)
-// Write the code that will log the output of 5 to the power of 7.
-console.log(5 ** 7)
-// Write the code that will log the whole number remainder of 33 divided by 6.
-console.log(33 % 6)
-// Write the code that will log the length of a string containing your name.
-var name = "Adrian"
-console.log(name.length)
-// Write the code that will log whether your string includes the letter "e"?
-console.log(name.includes("e"))
-// Write the code that will log the character at the first index of the string.
-console.log(name[0])
-// Write the code that will log the string in all uppercase letters.
-console.log(name.toUpperCase())
-//new line
+// // Write the code that will log the output of 34 added to 71.
+// console.log(34 + 71)
+// // Write the code that will log the output of 67 subtracted from 123.
+// console.log(123 - 67)
+// // Write the code that will log the output of 56 multiplied by 23.
+// console.log(56 * 23)
+// // Write the code that will log the output of 45 divided by 5.
+// console.log(45 / 5)
+// // Write the code that will log the output of 5 to the power of 7.
+// console.log(5 ** 7)
+// // Write the code that will log the whole number remainder of 33 divided by 6.
+// console.log(33 % 6)
+// // Write the code that will log the length of a string containing your name.
+// var name = "Adrian"
+// console.log(name.length)
+// // Write the code that will log whether your string includes the letter "e"?
+// console.log(name.includes("e"))
+// // Write the code that will log the character at the first index of the string.
+// console.log(name[0])
+// // Write the code that will log the string in all uppercase letters.
+// console.log(name.toUpperCase())
+// //new line
+
