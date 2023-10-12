5756da3cb852830cf4cfc7fa07be5654a1f5c743
zeniavarela <zvarela8@gmail.com>
2023-10-12 11:52:01 -0700

    second practice

--- javascript-kevin-zenia.js
+++ javascript-kevin-zenia.js
@@ -3,16 +3,37 @@
 // Write the code that will return a subset of the array containing only "chips" and "dip".
 // Write the code that will add "beans" to the "chips" and "dip" array.
 
-var groceryList = ["chips", "dip", "cookies"]
-console.log(groceryList.push("soda"))
-console.log(groceryList)
+// var groceryList = ["chips", "dip", "cookies"]
+// console.log(groceryList.push("soda"))
+// console.log(groceryList)
 
-var newList = ["granola"]
-var comboArrays = groceryList.concat(newList)
-console.log(comboArrays)
+// var newList = ["granola"]
+// var comboArrays = groceryList.concat(newList)
+// console.log(comboArrays)
 
-console.log(groceryList.slice(0,2))
+// console.log(groceryList.slice(0,2))
 
-var mySubset = groceryList.slice(0,2)
-console.log(mySubset.push("beans", "chips", "dip"))
-console.log(mySubset)
+// var mySubset = groceryList.slice(0,2)
+// console.log(mySubset.push("beans", "chips", "dip"))
+// console.log(mySubset)
+
+
+// Write the code that will add the number 0 to the beginning of the array.
+// Write the code that will add the number 12 to the end of the array.
+// Write the code that will remove the first number from the array.
+// Write the code that will add the number 0 to the beginning of the array without altering the original array. 
+// HINT: it's not .unshift You'll have to get creative! ;)
+
+// var numbers = [2, 4, 6, 8, 10]
+// console.log(numbers.unshift(0))
+// console.log(numbers)
+
+// console.log(numbers.push(12))
+// console.log(numbers)
+
+// console.log(numbers.shift())
+// console.log(numbers)
+
+// var notUnshift = [0]
+// var conCat = [notUnshift.concat(numbers)]
+// console.log(conCat)
