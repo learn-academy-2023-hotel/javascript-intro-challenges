b9d577e14fa9350a8c1bcde5428f1ee7e49aa3c0
JacksonPruett <jacksonc.pruett@gmail.com>
2023-10-12 11:22:57 -0700

    inital commit

---
+++ array-dd-jp.js
@@ -0,0 +1,12 @@
+var groceryList = ["chips", "dip", "cookies"]
+
+// groceryList.push("soda")
+// console.log(groceryList)
+
+// groceryList.push("soda")
+// groceryList.push("granola")
+
+// groceryList.pop()
+// groceryList.push("beans")
+
+console.log(groceryList)
