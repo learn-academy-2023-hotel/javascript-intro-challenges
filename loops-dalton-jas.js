942bbb8f6a7bc42ace7f127fecac45d61609981e
DaltonMcauliffe <dalton.j.mcauliffe97@gmail.com>
2023-10-12 15:39:39 -0700

    added some loops

---
+++ loops-dalton-jas.js
@@ -0,0 +1,16 @@
+// Dalton driving round 1
+
+//var count = 1 
+
+// for( let i = 0; i <= 20; i++) {
+//     count++
+//     console.log(count)
+// }
+
+for( let i = 0; i <= 20; i++) {
+    if(i % 2 !== 0 )    
+    console.log(i)
+}
+
+
+// Jas driving round 1
