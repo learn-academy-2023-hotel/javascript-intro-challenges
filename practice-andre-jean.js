3da90cd88ad94fb02d1dc2605e84560f5419e80d
LEARN Academy <learnacademy@MacBook-Air.local>
2023-10-12 12:05:10 -0700

    2nd commit

--- practice-andre-jean.js
+++ practice-andre-jean.js
@@ -15,3 +15,21 @@
 // var groceryList = ["chips","dip","cookies"]
 // console.log(groceryList.pop("cookies"),groceryList.push("beans"))
 // console.log(groceryList)
+
+//var numbers = [2, 4, 6, 8, 10]
+// Write the code that will add the number 0 to the beginning of the array.
+// var numbers = [2, 4, 6, 8, 10]
+// console.log(numbers.unshift(0))
+// console.log(numbers)
+// Write the code that will add the number 12 to the end of the array.
+// var numbers = [2, 4, 6, 8, 10]
+// console.log(numbers.push(12))
+// console.log(numbers)
+// Write the code that will remove the first number from the array.
+// var numbers = [2, 4, 6, 8, 10]
+// console.log(numbers.shift())
+// console.log(numbers)
+// Write the code that will add the number 0 to the beginning of the array without altering the original array. HINT: it's not .unshift You'll have to get creative! ;)
+var numbers = [2, 4, 6, 8, 10]
+console.log(numbers.push(0),numbers.sort((a, b) => a - b))
+console.log(numbers)
