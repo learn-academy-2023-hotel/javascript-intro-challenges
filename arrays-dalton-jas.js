9a3ed312043cdddd530b87dbf4750d8b93bf9ff5
Jas <jontel.a.scott@gmail.com>
2023-10-12 11:36:23 -0700

    Love Learning JS

---
+++ arrays-dalton-jas.js
@@ -0,0 +1,10 @@
+// Jas First Driver
+var groceryList = ["chips", "dip", "cookies"]
+    groceryList.push("soda")    
+console.log(groceryList)
+
+var granola = ["granola"]
+
+console.log(groceryList.concat(granola))
+
+// Dalton Second Driver
