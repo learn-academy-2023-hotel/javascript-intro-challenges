a04b79de8025565d665a4763736760f6f173d434
Jas <jontel.a.scott@gmail.com>
2023-10-12 16:38:35 -0700

    The pieces are coming together.

--- loops-dalton-jas.js
+++ loops-dalton-jas.js
@@ -7,9 +7,20 @@
 //     console.log(count)
 // }
 
-for( let i = 0; i <= 20; i+=2) {    
-    console.log(i)
-}
+// for( let i = 0; i <= 20; i+=2) {    
+//     console.log(i)
+// }
 
 
 // Jas driving round 1
+
+// for( let i = 1; i <= 20; i+=3) {    
+//     console.log(i)
+// }
+
+for(let i = 0; i<21; i++){
+    if(i % 2 !== 0 ){
+        console.log("ODD")
+    }   else{console.log(i)
+}
+}
