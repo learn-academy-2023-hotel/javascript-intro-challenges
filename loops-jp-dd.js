5100155f05b15bea85345f43cde4255fea8e8133
JacksonPruett <jacksonc.pruett@gmail.com>
2023-10-16 11:44:33 -0700

    second commit

--- loops-jp-dd.js
+++ loops-jp-dd.js
@@ -10,12 +10,15 @@
 const nums = [3, 57, -9, 20, 67];
 let biggerNumber = [0]
 
+
+
 for (let i = 0; i < nums.length; i++){
     if (nums[1] > nums[i+1]){
         
     }
 }
 
+const nums = [3, 57, -9, 20, 67]
 
 
 
@@ -45,12 +48,4 @@ for (let i = 0; i < nums.length; i++){
 
 
 
-// let largestNumber = nums[0];
 
-// for (let i = 1; i < nums.length; i++) {
-//     if (nums[i] > largestNumber) {
-//         largestNumber = nums[i];
-//     }
-// }
-
-// console.log(largestNumber);
