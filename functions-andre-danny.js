c8fe25159a862d7c85f2e18e8f260bd14643fce6
LEARN Academy <learnacademy@MacBook-Air.local>
2023-10-16 11:22:17 -0700

    first commit

---
+++ functions-andre-danny.js
@@ -0,0 +1,31 @@
+// const myFunction = (marco) => {
+// return `${marco}`
+// }
+// console.log(myFunction("polo"))
+
+// const myGreeting = (myGreeting) => {
+//     return `Welcome ${myGreeting}`
+// }
+// console.log(myGreeting("person's name"))
+
+// const number = (OddOrEven) => {
+//     if(OddOrEven % 2 !== 0){
+//         return `This number is odd`
+//     } 
+//     else{
+//         return `This number is even`
+//     }  
+// }
+// console.log(number(6))
+
+// const number = (triple) => {
+// return `${triple}` * 3 // why wont error happen without back tics while not needing a string
+// }
+// console.log(number(8))
+
+// const bigNumber = (num1 , num2) => {
+//     return `${num1 * num2}`
+// }
+// console.log(bigNumber(5,8))
+
+
