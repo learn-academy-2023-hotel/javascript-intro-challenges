6eba767fa7c733096d94a3f3cb196391a76b50ad
JacksonPruett <jacksonc.pruett@gmail.com>
2023-10-17 04:41:39 -0700

    more challenges done

--- functions-jackson-ron.js
+++ functions-jackson-ron.js
@@ -8,13 +8,22 @@
 // }
 // console.log(greetings("Jackson"))
 
-const oddOrEven = (aNumbers) => {
-    if(aNumbers % 2 === 0) {
-      return `${aNumbers} is even`
-    } else if (aNumbers % 2 === 1){
-      return `${aNumbers} is odd`
-    }
-  }
+// const oddOrEven = (aNumbers) => {
+//     if(aNumbers % 2 === 0) {
+//       return `${aNumbers} is even`
+//     } else if (aNumbers % 2 === 1){
+//       return `${aNumbers} is odd`
+//     }
+//   }
 
-console.log(oddOrEven(404))
-console.log(oddOrEven(55))
+// console.log(oddOrEven(404))
+// console.log(oddOrEven(55))
+
+// const all = (num1 / num2)
+const divisibleBy = (num1, num2) => {
+  if (divisibleBy % 2 === 0){
+ return `${num1} is divisible by ${num2}!`
+ }
+}
+
+console.log(divisibleBy(10, 5))
